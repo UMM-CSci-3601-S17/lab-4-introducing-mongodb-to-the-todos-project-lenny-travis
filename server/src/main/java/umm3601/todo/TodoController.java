54dca74f53e47850b0d013db00b79f8d8b781455
Dogxx000 <scot0530@morris.umn.edu>
2017-02-21 13:43:28 -0600

    Begin server todocontroller
    add Todo.java
    add TodoController.java
    issue #3
    issue #6

---
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,69 @@
+package umm3601.todo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class TodoController {
+
+    private final MongoCollection<Document> todoCollection;
+
+    public TodoController() throws IOException {
+        // Set up our server address
+        // (Default host: 'localhost', default port: 27017)
+        // ServerAddress testAddress = new ServerAddress();
+
+        // Try connecting to the server
+        //MongoClient mongoClient = new MongoClient(testAddress, credentials);
+        MongoClient mongoClient = new MongoClient(); // Defaults!
+
+        // Try connecting to a database
+        MongoDatabase db = mongoClient.getDatabase("test");
+
+        todoCollection = db.getCollection("todos");
+    }
+
+    // List todos by owner
+    public String listTodosByOwner(Map<String, String[]> queryParams) {
+        Document filterDoc = new Document();
+
+        if (queryParams.containsKey("owner")) {
+            int targetOwner = Integer.parseInt(queryParams.get("owner")[0]);
+            filterDoc = filterDoc.append("owner", targetOwner);
+        }
+
+        FindIterable<Document> matchingUsers = todoCollection.find(filterDoc);
+
+        return JSON.serialize(matchingUsers);
+    }
+
+    // Get a single todo
+    public String getTodo(String id) {
+        FindIterable<Document> jsonUsers
+                = todoCollection
+                .find(eq("_id", new ObjectId(id)));
+
+        Iterator<Document> iterator = jsonUsers.iterator();
+
+        Document user = iterator.next();
+
+        return user.toJson();
+    }
+
+
+}
