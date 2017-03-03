dcb9cecd5f0a6e7fc1c9a747e834c284d10e862f
Jeiloss <15thirty>
2017-03-02 20:54:25 -0600

    Trying to get Client to show things
    Does not display any todos.
    The website runs again, so that's a plus.
    There are probably no todos being showed because the client is asking the server to talk to the mongoDB
    
    #4

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
 
-
 import { AppComponent }         from './app/app.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
 import { HomeComponent} from './app/home/home.component';
 import { KittensComponent }   from './app/kittens/kittens.component';
 import { UserListComponent } from './app/users/user-list.component';
-import { TodoListComponent } from './app/todos/todo-list.component';
 import { UserListService } from './app/users/user-list.service';
+import { TodoListComponent } from './app/todos/todo-list.component';
+import { TodoListService } from "./app/todos/todo-list.service";
 import { routing } from './app/app.routes';
 import { FormsModule } from '@angular/forms';
 
@@ -32,7 +32,7 @@ import { PipeModule } from './pipe.module';
         UserListComponent,
         TodoListComponent
     ],
-    providers: [ UserListService ],
+    providers: [ UserListService, TodoListService ],
     bootstrap: [ AppComponent ]
 })
 
