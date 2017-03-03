2ff3dc1ffc981e39fb3adbc4bc9cc29fe4deba15
Jeiloss <15thirty>
2017-03-02 20:34:56 -0600

    Trying to get Client to show things !broken commit
    #4

--- client/src/app/users/user-list.component.ts
+++ client/src/app/users/user-list.component.ts
@@ -5,7 +5,7 @@ import { FilterBy } from "./filter.pipe";
 
 @Component({
     selector: 'user-list-component',
-    templateUrl: 'todo-list.component.html',
+    templateUrl: 'user-list.component.html',
     providers: [ FilterBy ]
 })
 
